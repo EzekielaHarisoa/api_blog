336f5e06b73af5894e57dfc9b2301d08ab0d5bea
Ezekiela Harisoa <ezekielaharisoa825@gmail.com>
2026-05-13 12:39:23 +0300

    fix:route for comments

--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const commentController = require("../controllers/comment.controller");
 const authMiddleware = require("../middlewares/middleware");
 
-router.get("/posts/:postId/comments",authMiddleware, commentController.getAllCommentsByPost);
+router.get("/posts/:postId/comments", commentController.getAllCommentsByPost);
 
 router.post("/posts/:postId/comments", authMiddleware, commentController.createComment);
 
-router.put("/comments/:id", authMiddleware, commentController.editComment);
+router.put("/:id", authMiddleware, commentController.editComment);
 
-router.delete("/comments/:id", authMiddleware, commentController.deleteComment);
+router.delete("/:id", authMiddleware, commentController.deleteComment);
 
 module.exports = router;
